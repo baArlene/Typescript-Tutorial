24c2dca4a3188a1c75a8152d02ea28dce7972348
unknown <carloschinyuku@gmail.com>
2024-03-26 22:07:07 +0200

    first commit

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -10,3 +10,23 @@ isAdult = !isAdult
 
 console.log(isAdult);
 
+let tax: number | string = 15;
+tax = 100;
+tax = 'R100'
+
+
+let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing';
+orderStatus = 'shipped';
+orderStatus = 'delivered';
+
+let discount: string | number = 10;
+discount = '10%'
+
+let prices:number[] = [100, 90, 75]
+
+let fruit:string[] = ['apple', 'orange']
+
+
+let emptyValues:number[] = [];
+
+let names = ['peter', 'john', 'regis', 10]
